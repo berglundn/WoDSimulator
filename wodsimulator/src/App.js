1638c255f9961b63c78f3107e272446ae0c0ffa1
berglundn <nathanjberglund@gmail.com>
2025-05-30 11:16:32 -0500

    And so it begins

---
+++ wodsimulator/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import NpcGenerator from './Components/M20/NpcGenerator'
+
+function App() {
+  return (
+    <div className="App">
+      <NpcGenerator/>
+    </div>
+  );
+}
+
+export default App;
---
+++ wodsimulator/src/DataProviders/RandomGenerator.js
@@ -0,0 +1,79 @@
+import Constants from "./Mage/Constants"
+class RandomGenerator{
+    static getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+    static getArrayItemEvenOdds(array){
+        return array[this.getRandomInt(array.length-1)];
+   }
+
+    static getRandomName(){
+        return "Jared Ledsworth";
+    }
+    
+    static getRandomArchetype(){
+        return this.getArrayItemEvenOdds(Constants.ArchetypesArray);
+    }
+    
+    static getRandomEssence(){
+        return this.getArrayItemEvenOdds(Constants.Essences);
+    }
+
+    static getRandomAffiliation(){
+        return this.getArrayItemEvenOdds(Constants.Affiliations);
+    }
+
+    static getRandomSect(affiliation){
+        var sect = "ERROR";
+        if (affiliation === "Disparate"){
+            sect = this.getArrayItemEvenOdds(Constants.DisparateSects)
+        } else if (affiliation === "Marauder"){
+            sect = "Marauder"
+        } else if (affiliation === "Nephandi"){
+            sect = this.getArrayItemEvenOdds(Constants.NephandiSects)
+        } else if (affiliation === "Technocracy"){
+            sect = this.getArrayItemEvenOdds(Constants.TechnocracySects)
+        } else {
+            sect = this.getArrayItemEvenOdds(Constants.TechnocracySects)
+        }
+        return sect;
+    }
+    static getRandomAttributeWorth(){
+        var probability = this.getRandomInt(100);
+        var worth;
+        if (probability <= 20){
+            worth = 1;
+        } else if (probability > 20 && probability <= 40){
+            worth = 2;
+        } else if (probability > 40 && probability <= 60){
+            worth = 3;
+        } else if (probability > 60 && probability <= 80){
+            worth = 4
+        } else {
+            worth = 5;
+        }
+        return worth;
+    }
+
+    static getRandomAbilityWorth(){
+        var probability = this.getRandomInt(100);
+        var worth;
+        if (probability <= 25){
+            worth = 0;
+        } else if (probability > 25 && probability <= 45){
+            worth = 1;
+        } else if (probability > 45 && probability <= 70){
+            worth = 2;
+        } else if (probability > 70 && probability <= 85){
+            worth = 3;
+        } else if (probability > 85 && probability <= 95){
+            worth = 4
+        } else {
+            worth = 5;
+        }
+        return worth;
+    }
+}
+
+export default RandomGenerator;
